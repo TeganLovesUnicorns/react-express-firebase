a81d1f4739cd4a19311f47ff26e51e41f092afef
flyingguitarman <flyingguitarman@gmail.com>
2017-11-30 15:00:42 -0600

    fallback commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { ServerRouter } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
   (
-    <BrowserRouter>
+    <ServerRouter>
       <App />
-    </BrowserRouter>
+    </ServerRouter>
   ), document.getElementById('root'),
 );
