ef850f6169005aad27d8e73bbdbd2a6a8d7b288d
flyingguitarman <flyingguitarman@gmail.com>
2017-12-01 17:27:15 -0600

    pre isomorphic-loader

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-// const amosLee = require('../images/amos_lee_2017.jpg');
+const amosLee = require('../images/amos_lee_2017.jpg');
 
 const Home = () => (
   <div className="home">
-    {/* <img src={amosLee} alt="Amos Lee" /> */}
+    <img src={amosLee} alt="Amos Lee" />
     <h1>Welcome</h1>
   </div>
 );
