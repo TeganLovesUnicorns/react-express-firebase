29f4cd9d9e491f0c122d6c9917570b80037a21ca
flyingguitarman <flyingguitarman@gmail.com>
2017-12-20 00:34:05 -0600

    images

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Home = () => (
   <div className="home">
     {/* <img src={amosLee} alt="Amos Lee" /> */}
-    <img src="http://danielrehbein.com/img/homeslider/1.jpg" alt="Amos Lee" />
+    <img src="https://firebasestorage.googleapis.com/v0/b/eclipse-lighting-solutions.appspot.com/o/img%2FAL1701.jpg?alt=media&token=e66b31ea-4dfb-4af4-ba1f-c9b885581b8a" alt="Amos Lee" />
     <h1>Welcome</h1>
   </div>
 );
