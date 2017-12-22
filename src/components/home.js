0254348359172e62ebbbba98363eda4fd5926811
flyingguitarman <flyingguitarman@gmail.com>
2017-12-22 13:24:27 -0600

    before carousel json

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
-// const amosLee = require('../images/amos_lee_2017.jpg');
+import { Carousel } from 'react-bootstrap';
+import data from '../../res/imgdb/home.json';
 
 const Home = () => (
   <div className="home">
-    {/* <img src={amosLee} alt="Amos Lee" /> */}
-    <img src="https://firebasestorage.googleapis.com/v0/b/eclipse-lighting-solutions.appspot.com/o/img%2FAL1701.jpg?alt=media&token=e66b31ea-4dfb-4af4-ba1f-c9b885581b8a" alt="Amos Lee" />
-    <h1>Welcome</h1>
+    <Carousel interval={5000}>
+      <Carousel.Item>
+        <img width={750} height={500} alt="Amos Lee 1" src="https://firebasestorage.googleapis.com/v0/b/eclipse-lighting-solutions.appspot.com/o/img%2FAL1701.jpg?alt=media&token=e66b31ea-4dfb-4af4-ba1f-c9b885581b8a" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width={750} height={500} alt="Amos Lee 2" src="https://firebasestorage.googleapis.com/v0/b/eclipse-lighting-solutions.appspot.com/o/img%2FAL1702.jpg?alt=media&token=12b2e38b-61d7-48ad-a34a-1e992f24fead" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width={750} height={500} alt="Amos Lee 3" src="https://firebasestorage.googleapis.com/v0/b/eclipse-lighting-solutions.appspot.com/o/img%2FAL1703.jpg?alt=media&token=0cf3d18c-c5e1-4222-b51f-a084b1683b88" />
+      </Carousel.Item>
+    </Carousel>
   </div>
+  
 );
 
 export default Home;
